47cd878f0ff943cace83c6695542dce6e709c810
Lucas Dias <lucascmdias@gmail.com>
2020-08-03 18:36:34 -0300

    k

--- Init/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-
-const connection = new Sequelize('teste1','root','Lp@f1944', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = connection
