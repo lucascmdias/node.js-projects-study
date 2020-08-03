80dc30005f0876dcf02b7d257c54a072313a67a2
Lucas Dias <lucascmdias@gmail.com>
2020-08-03 18:38:07 -0300

    ajeitando

--- Projeto-ejs-get-post/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-
-const connection = new Sequelize('projeto-inicial','root', 'Lp@f1944',{
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = connection
